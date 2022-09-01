3b65e887f0b248aad55a5d5272a8fce39da776e1
turbovladimir <36790229+turbovladimir@users.noreply.github.com>
2022-09-01 14:08:14 +0300

    Update Cryptor.php

--- src/Cryptor.php
+++ src/Cryptor.php
@@ -5,25 +5,33 @@ class Cryptor
 {
     const CIPHER_METHOD = 'aes-128-ctr';
 
-    /**
-     * @param string $string
-     * @param string $salt
-     * @return string
-     */
-    public function encrypt(string $string, string $salt) : string {
-        $encIv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::CIPHER_METHOD));
+    private $salt;
 
+    public function __construct(?string $salt = null) 
+    {
+        $this->salt = $salt;
+    }
+    
+    public function encrypt(string $string, ?string $salt = null) : string {
+        $encIv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::CIPHER_METHOD));
+        $salt = $salt ?? $this->salt;
+        
+        if (!$salt) {
+            throw new \RuntimeException('Cannot fetch salt...Operation fail!');
+        }
+        
         return openssl_encrypt($string, self::CIPHER_METHOD, $salt, 0, $encIv). "::" . bin2hex($encIv);
     }
-
-    /**
-     * @param string $string
-     * @param $salt
-     * @return string
-     */
-    public function decrypt(string $string, $salt) : string {
+    
+    public function decrypt(string $string, ?string $salt = null) : string {
         if (!strpos($string, '::')) {
             throw new \InvalidArgumentException('Are you sure what this row was encrypt early?');
+        }
+
+        $salt = $salt ?? $this->salt;
+
+        if (!$salt) {
+            throw new \RuntimeException('Cannot fetch salt...Operation fail!');
         }
 
         list($cryptedToken, $encIv) = explode("::",$string);
